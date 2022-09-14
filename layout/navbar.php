d1694e1bb0f13d7621097a90535fcc46ef6c56d2
John Leo Samante <johnleomuitsamante@gmail.com>
2022-09-14 13:15:02 +0800

    Changed logout modal link

--- layout/navbar.php
+++ layout/navbar.php
@@ -20,7 +20,7 @@
         <img class="img-profile rounded-circle" src="<?php echo $_SESSION['userimage']; ?>">
       </a>
 
-      <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in p-0" aria-labelledby="userDropdown">
+      <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in py-1" aria-labelledby="userDropdown">
         <a class="dropdown-item" href="#">
           <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i> Profile
         </a>
@@ -31,7 +31,7 @@
           <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i> Activity Log
         </a>
         <div class="dropdown-divider m-0"></div>                     
-        <a class="dropdown-item" href="<?php echo GetURL(); ?>/logout">
+        <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
           <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i> Logout
         </a>
       </div>
