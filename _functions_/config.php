eb0fb6d2c98aaa734df26ec8905ad0373ae5927e
John Leo Samante <johnleomuitsamante@gmail.com>
2022-10-20 19:50:58 +0800

    Project name changed.

--- _functions_/config.php
+++ _functions_/config.php
@@ -2,15 +2,15 @@
 # _functions_/config.php
 
 # CONSTANTS
-define('DB_SERVER', '124.106.213.148');
+define('DB_SERVER', 'localhost');
 define('DB_USER', 'web_user_2022');
 define('DB_PASSWORD', 'u78Ee71FEY&T6krB9TCu');
 define('DB_NAME', 'sdodipologdb');
 define('DB_SERVER_PORT', '3306');
 define('PASSWORD_SALT_PREFIX', '5d0');
 define('PASSWORD_SALT_SUFFIX', 'd1p0706');
-define('TITLE', 'DepEd Dipolog City Data Management System');
-define('ALIAS', 'DCDMS');
+define('TITLE', 'DepEd Dipolog City Data Management Information System');
+define('ALIAS', 'DCDMIS');
 define('DEPARTMENT', 'Department of Education');
 define('DIVISION', 'Dipolog City Schools Division');
 define('CITY', 'Dipolog City');
